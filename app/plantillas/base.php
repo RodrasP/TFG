6a1f26cd60c267e8d1cc7f00a7f7d463046bf1a7
Rodrigo <rodrigoperezserrano28@gmail.com>
2023-06-05 00:08:26 +0200

    arreglos

--- app/plantillas/base.php
+++ app/plantillas/base.php
@@ -28,97 +28,101 @@
     </div>
   </header>
 
-  <nav>
-    <a href="index.php?ctl=inicio" class="enlace">Inicio</a> |
-    <a href="index.php?ctl=contacto" class="enlace">Contacto</a> |
-    <?php if ($_SESSION['usuario'] === 'anonimo') : ?>
-      <a href="index.php?ctl=iniSes" class="enlace">Iniciar Sesion</a> |
+  <?php if ($_SESSION['usuario'] === 'anonimo') : ?>
+    <nav style="padding: 16px;">
     <?php else : ?>
-      <a href="index.php?ctl=reservar" class="enlace">Reservar</a> |
-
-      <?php if ($_SESSION['rol'] === 'admin') : ?>
-        <a href="index.php?ctl=users" class="enlace">Usuarios</a> |
-      <?php endif ?>
-
-      <div class="dropdown">
-        <button class="dropbtn"><?= $_SESSION['nombre_usuario'] ?></button>
-        <div class="dropdown-content">
-          <form method="post" action="index.php">
-            <input type="hidden" name="ctl" value="cerrarSes">
-            <input type="submit" value="Cerrar Sesion" name="ok">
-          </form>
-        </div>
-      </div>
-
-    <?php endif; ?>
-
-
-
-  </nav>
-
-
-
-
-  <div id="contenido">
-
-
-    <?= $contenido ?>
-  </div>
-  <footer>
-    <div class="navegacion">
-
       <nav>
-        <ul>
-          <li>
-            <a href="index.php?ctl=inicio" class="enlace">Inicio</a>
-          </li>
-          <li>
-            <a href="index.php?ctl=contacto" class="enlace">Contacto</a>
-          </li>
-          <?php if ($_SESSION['usuario'] === 'anonimo') : ?>
-            <li>
-              <a href="index.php?ctl=iniSes" class="enlace">Iniciar Sesion</a>
-            </li>
-          <?php else : ?>
-            <li>
-              <a href="index.php?ctl=reservar" class="enlace">Reservar</a>
-            </li>
-            <?php if ($_SESSION['rol'] === 'admin') : ?>
-              <li>
-                <a href="index.php?ctl=users" class="enlace">Usuarios</a>
-              </li>
-            <?php endif ?>
-          <?php endif; ?>
-        </ul>
+      <?php endif ?>
+      <a href="index.php?ctl=inicio" class="enlace">Inicio</a> |
+      <a href="index.php?ctl=contacto" class="enlace">Contacto</a> |
+      <?php if ($_SESSION['usuario'] === 'anonimo') : ?>
+        <a href="index.php?ctl=iniSes" class="enlace">Iniciar Sesion</a> |
+      <?php else : ?>
+        <a href="index.php?ctl=reservar" class="enlace">Reservar</a> |
+
+        <?php if ($_SESSION['rol'] === 'admin') : ?>
+          <a href="index.php?ctl=users" class="enlace">Usuarios</a> |
+        <?php endif ?>
+
+        <div class="dropdown">
+          <button class="dropbtn"><?= $_SESSION['nombre_usuario'] ?></button>
+          <div class="dropdown-content">
+            <form method="post" action="index.php">
+              <input type="hidden" name="ctl" value="cerrarSes">
+              <input type="submit" value="Cerrar Sesion" name="ok">
+            </form>
+          </div>
+        </div>
+
+      <?php endif; ?>
+
+
+
       </nav>
-    </div>
 
-    <div class="nombre">
-      <h1>VecinosEnRed</h1>
-      <p>Nos vemos en la pista</p>
-    </div>
-    <div class="contacto-footer">
-      <h3 style="color: white;">Contacto</h3>
-      <ul>
-        <li style="color: white;">
-          <span>Teléfono:</span> +34 612 345 678
-        </li>
-        <li style="color: white;">
-          <span>Correo electrónico:</span> info@VecinosEnRed.com
-        </li>
-      </ul>
-    </div>
 
-  </footer>
-  <script>
-    // Seleccionar el elemento con la clase dropdown-content
-    const dropbtn = document.querySelector('.dropbtn');
-    dropbtn.addEventListener('click', function() {
 
-      window.location.replace('index.php?ctl=perfil');
-     
-    });
-  </script>
+
+      <div id="contenido">
+
+
+        <?= $contenido ?>
+      </div>
+      <footer>
+        <div class="navegacion">
+
+          <nav>
+            <ul>
+              <li>
+                <a href="index.php?ctl=inicio" class="enlace">Inicio</a>
+              </li>
+              <li>
+                <a href="index.php?ctl=contacto" class="enlace">Contacto</a>
+              </li>
+              <?php if ($_SESSION['usuario'] === 'anonimo') : ?>
+                <li>
+                  <a href="index.php?ctl=iniSes" class="enlace">Iniciar Sesion</a>
+                </li>
+              <?php else : ?>
+                <li>
+                  <a href="index.php?ctl=reservar" class="enlace">Reservar</a>
+                </li>
+                <?php if ($_SESSION['rol'] === 'admin') : ?>
+                  <li>
+                    <a href="index.php?ctl=users" class="enlace">Usuarios</a>
+                  </li>
+                <?php endif ?>
+              <?php endif; ?>
+            </ul>
+          </nav>
+        </div>
+
+        <div class="nombre">
+          <h1>VecinosEnRed</h1>
+          <p>Nos vemos en la pista</p>
+        </div>
+        <div class="contacto-footer">
+          <h3 style="color: white;">Contacto</h3>
+          <ul>
+            <li style="color: white;">
+              <span>Teléfono:</span> +34 612 345 678
+            </li>
+            <li style="color: white;">
+              <span>Correo electrónico:</span> info@VecinosEnRed.com
+            </li>
+          </ul>
+        </div>
+
+      </footer>
+      <script>
+        // Seleccionar el elemento con la clase dropdown-content
+        const dropbtn = document.querySelector('.dropbtn');
+        dropbtn.addEventListener('click', function() {
+
+          window.location.replace('index.php?ctl=perfil');
+
+        });
+      </script>
 </body>
 
 </html>
